39ea901654a4de10af32d13b4fd80e0d7bc4f7e9
mikloshevich <pavkatc@gmail.com>
2021-09-09 19:58:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -86,9 +86,9 @@
         }
         drawCircles(circleRadius);
 
-        let segments = array.length;
+        let segments = array.length/2;
         let radius = (cnv.width + array[0]*8) / 45 + radiusOffset;
-        let angle = (Math.PI * 5) / segments;
+        let angle = (Math.PI * 3) / segments;
 
         for (let i = 0; i < segments; i++) {
             let barHeight = array[i] > 0 ? array[i]/1.5 + (cnv.width*0.005) : 1;
